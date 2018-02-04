dcc54748c061c28fe8154b81c2816995460a9815
Frank Wilkerson <FWilkerson@ad.exetered.com>
2018-02-03 19:17:58 -0700

    linter & build process

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 export const routerModel = {
   activeView: null,
-  route: history.state || {path: window.location.pathname},
-}
+  route: history.state || { path: window.location.pathname }
+};
 
 function matchRoute(route) {
   const currentPath = window.location.pathname;
   if (route.exact) {
     return currentPath === route.path;
   }
-  return currentPath.match(/[^\/]+/g)[0] === route.path.match(/[^\/]+/g)[0];
+  return currentPath.match(/[^/]+/g)[0] === route.path.match(/[^/]+/g)[0];
 }
 
 export function Route(props) {
@@ -16,43 +16,43 @@ export function Route(props) {
 }
 
 export function Router(props) {
-  if (!props.children) return; // Error?
+  if (!props.children) return;
   if (!window.onpopstate) window.onpopstate = props.routeChanged;
   const match = props.children.find(matchRoute);
   if (match) {
     return match.view(props.model);
-  } else {
-    // Not Found?
   }
 }
 
 export function createRouterActions(getModel, setModel) {
   return {
     handleRouteChanged(event) {
-      const {activeView} = getModel();
+      const { activeView } = getModel();
       setModel({
-        route: event.state || {path: window.location.pathname},
-        activeView: null,
+        route: event.state || { path: window.location.pathname },
+        activeView: null
       });
       if (activeView && activeView.didUnmount) {
         activeView.didUnmount();
       }
     },
     setActiveView(activeView) {
-      setModel({activeView});
+      setModel({ activeView });
       if (activeView.didMount) {
         activeView.didMount();
       }
     },
     setRoute(pathname) {
-      const {activeView, route} = getModel();
-      if (pathname === route.path) return;
-      let newRoute = {path: pathname};
-      history.pushState(newRoute, '', pathname);
-      setModel({route: newRoute, activeView: null});
+      const { activeView, route } = getModel();
+      if (pathname === route.path) {
+        return;
+      }
+      const newRoute = { path: pathname };
+      history.pushState(newRoute, "", pathname);
+      setModel({ route: newRoute, activeView: null });
       if (activeView && activeView.didUnmount) {
         activeView.didUnmount();
       }
-    },
-  }
+    }
+  };
 }
